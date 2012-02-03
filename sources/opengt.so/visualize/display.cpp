ef4ef340e205f874f37cba8cf9139c853ee06282
algorithman <algorithman@7d856203-d9e4-4e18-aee0-bf315c1b057e>
2012-02-03 10:49:21 +0000

    code for management of an X11 window (doesn't compile right now, but will be used for displaying a graph, later)
    git-svn-id: svn://svn.code.sf.net/p/opengraphtheory/code/Trunk@18 7d856203-d9e4-4e18-aee0-bf315c1b057e

---
+++ sources/opengt.so/visualize/display.cpp
@@ -0,0 +1,59 @@
+
+#include "DisplayWindow.h"
+#include "../opengt.h"
+#include <iostream>
+#include <sstream>
+#include <unistd.h> // for usleep
+using namespace std;
+using namespace OpenGraphtheory;
+
+int sleep_nanosec = 41666; // must be less than 1,000,000
+int update_interval = 100; // read this many changes before updating the window
+
+int main(int argc, char** argv) {
+	DisplayWindow Win(800,600,"Open-Graphtheory.org Display Window");
+	Graph G;
+	cin >> G;
+
+	string input;
+	do {
+		cin >> input;
+		if(!cin.good())
+			break;
+
+		if(input == "F") {
+			// draw graph
+			Win.Clear();
+			for(Graph::VertexIterator v = G.BeginVertices(); v != G.EndVertices(); v++) {
+				Win.Circle(v.GetX(), v.GetY(), 4);
+				for(Graph::VertexIterator w = v.BeginAdjacentVertices(); w != v.EndAdjacentVertices(); w++) {
+					Win.Line(v.GetX(),v.GetY(),w.GetX(),w.GetY());
+				}
+			}
+			Win.Flush();
+			usleep(sleep_nanosec);
+		} else if(input == "X") {
+			int vint;
+			cin >> vint;
+			Graph::VertexIterator vit = G.GetVertexByXML_ID(vint);
+			float temp;
+			cin >> temp;
+			if(vit != G.EndVertices()) {
+				 vit.SetX(temp);
+			}
+		} else if(input == "Y") {
+			int vint;
+			cin >> vint;
+			Graph::VertexIterator vit = G.GetVertexByXML_ID(vint);
+			float temp;
+			cin >> temp;
+			if(vit != G.EndVertices()) {
+				 vit.SetY(temp);
+			}
+		}
+
+	} while(input != "quit");
+
+	Win.WaitUntilClosed();
+	return 0;
+}
