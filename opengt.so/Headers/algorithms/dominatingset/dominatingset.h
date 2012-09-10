14fe7eb5631e266bf1e780b83c7d5d3226bad858
algorithman <algorithman@7d856203-d9e4-4e18-aee0-bf315c1b057e>
2012-09-10 14:41:40 +0000

    git-svn-id: svn://svn.code.sf.net/p/opengraphtheory/code/Trunk@77 7d856203-d9e4-4e18-aee0-bf315c1b057e

---
+++ opengt.so/Headers/algorithms/dominatingset/dominatingset.h
@@ -0,0 +1,32 @@
+
+#ifndef __OPENGRAPHTHEORY_ALGORITHMS_DOMINATINGSET_DOMINATINGSET_H
+    #define __OPENGRAPHTHEORY_ALGORITHMS_DOMINATINGSET_DOMINATINGSET_H
+
+    #include "../algorithm.h"
+    #include<string>
+    #include<set>
+    #include<vector>
+
+    namespace OpenGraphtheory
+    {
+        namespace Algorithms
+        {
+            class AlgorithmDOMINATINGSET : public Algorithm
+            {
+                public:
+                    void Run(Graph &G, std::vector<std::string> parameters);
+                    bool FindDominatingSet(Graph& G, std::set<Graph::VertexIterator>& DominatingSet, unsigned int k, bool VerticesCanDominateThemselves = true);
+                    void AddDominatingSet(Graph &G, std::string DominatingSetName, bool VerticesCanDominateThemselves = true);
+
+                protected:
+                    static FactoryRegistrator<Algorithm> AlgorithmDominatingSetRegistrator;
+                    bool TestDominatingSet(std::map<Graph::VertexIterator,int>& Dominators, std::set<Graph::VertexIterator>& Undominated,
+                                           std::set<Graph::VertexIterator>& Excluded, std::set<Graph::VertexIterator>& DominatingSet,
+                                           unsigned int k, bool VerticesCanDominateThemselves = true);
+            };
+        }
+    }
+
+#endif
+
+
